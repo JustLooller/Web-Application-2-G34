aba578fb0656c3ce40e0a36bebcc913cae5e6a2c
Giacomo Demattia <giacomo.demattia@studenti.polito.it>
2023-04-03 11:54:59 +0200

    /products/{ean} PathVariable validation

--- server/src/main/kotlin/it/polito/wa2/g34/server/ProductController.kt
+++ server/src/main/kotlin/it/polito/wa2/g34/server/ProductController.kt
@@ -1,10 +1,13 @@
 package it.polito.wa2.g34.server
 
+import jakarta.validation.constraints.Size
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@Validated
 class ProductController(
     private val productService: ProductService
 ) {
@@ -12,8 +15,9 @@ class ProductController(
     fun getAll(): List<ProductDTO> {
         return productService.getAll()
     }
+
     @GetMapping("/products/{ean}")
-    fun getProduct(@PathVariable ean: String): ProductDTO? {
+    fun getProduct(@PathVariable @Size(min=13,max=13) ean: String ): ProductDTO? {
         return productService.getProduct(ean)
     }
 }
