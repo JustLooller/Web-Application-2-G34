f2a7f5dc8ba308de9fb1912e166dd003f1d02ba7
marcochessa <marco.chessa@studenti.polito.it>
2023-04-06 18:57:35 +0200

    profile v1

---
+++ server/src/main/kotlin/it/polito/wa2/g34/server/profile/ProfileController.kt
@@ -0,0 +1,43 @@
+package it.polito.wa2.g34.server.profile
+
+import jakarta.validator.Email
+import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+
+@RestController
+@Validated
+class ProfileController(
+    private val profileService: ProfileService
+) {
+    @GetMapping("/profiles/{email}")
+    fun getProfile(@PathVariable @Email email: String ): ProfileDTO? {
+        return profileService.getProfile(email)?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+    @PostMapping("/profiles")
+    fun postProfile(@RequestBody newProfile: Profile): ProfileDTO?{
+        return if(getProfile(newProfile.email) == null)
+            profileService.postProfile(newProfile);
+        else
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+    }
+
+    @PutMapping("/profiles{email}")
+    fun putProfile(@RequestBody editedProfile: Profile, @PathVariable @Email email: String ): ProfileDTO?{
+        return if(getProfile(editedProfile.email) != null) {
+            editedProfile.email = email;
+            profileService.postProfile(editedProfile);
+        }
+        else throw ResponseStatusException(HttpStatus.NOT_FOUND)
+    }
+
+}
+
+
