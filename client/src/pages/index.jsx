047db3174ecca92391c2dbb5d5962972f4c959e6
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2023-04-10 20:54:42 +0200

    updated index page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -11,12 +11,14 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main>
-                <div id="products" >
-                    <Link className="text-3xl" href={"/products/"}> Vai alla sezione Prodotti </Link>
+            <main className={"p-5 flex flex-wrap justify-center  align-middle w-screen h-screen self-center"}>
+                <div id="products" className={"h-1/2 md:h-full flex justify-center w-full md:w-1/2 mb-5px text-center"}>
+                    <Link className="text-3xl self-center underline" href={"/products/"}> Vai alla sezione Prodotti </Link>
                 </div>
-                <div id="accounts">
-                    <Link href={"/profiles/"}> Vai alla sezione Profili </Link>
+                <div id="accounts" className={"h-1/2 md:h-full flex justify-center w-full md:w-1/2 mb-5px text-center"}>
+                    <Link href={"/profiles/"} className="text-3xl underline self-center">
+                        Vai alla sezione Profili
+                    </Link>
                 </div>
             </main>
         </>
