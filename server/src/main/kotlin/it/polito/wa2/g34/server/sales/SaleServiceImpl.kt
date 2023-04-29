1f570d9f835ca07db658db40a88a86c817983a86
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2023-04-29 18:40:50 +0200

    breaking: lab3 impl pt2 (jb code with me)

---
+++ server/src/main/kotlin/it/polito/wa2/g34/server/sales/SaleServiceImpl.kt
@@ -0,0 +1,19 @@
+package it.polito.wa2.g34.server.sales
+
+import it.polito.wa2.g34.server.ticketing.dto.TicketDTO
+import kotlin.jvm.optionals.getOrDefault
+
+class SaleServiceImpl(
+    private val saleRepository: SaleRepository
+) : SaleService{
+
+    override fun getSaleByTicket(ticket: TicketDTO): SaleDTO {
+        var found = saleRepository.findById(ticket.sale_id);
+        if (found.isPresent) {
+            return found.get().toDTO()
+        } else {
+//            throw SaleNotFoundException()
+            TODO("throw SaleNotFoundException()")
+        }
+    }
+}
---
+++ server/src/main/kotlin/it/polito/wa2/g34/server/ticketing/service/StateHistoryService.kt
@@ -0,0 +1,11 @@
+package it.polito.wa2.g34.server.ticketing.service
+
+import it.polito.wa2.g34.server.profile.ProfileDTO
+import it.polito.wa2.g34.server.ticketing.dto.TicketDTO
+import it.polito.wa2.g34.server.ticketing.dto.UpdateTicketStatusDTO
+import it.polito.wa2.g34.server.ticketing.entity.StateHistory
+
+interface StateHistoryService {
+    fun getHistory(ticket: TicketDTO) : List<StateHistory>;
+    fun updateState(update: UpdateTicketStatusDTO);
+}
