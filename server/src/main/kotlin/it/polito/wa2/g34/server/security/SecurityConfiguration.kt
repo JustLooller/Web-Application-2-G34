09c69c5df5e3098ff86f9ad3766e4d8111672973
Giacomo Demattia <giacomo.demattia@studenti.polito.it>
2023-06-07 18:58:09 +0200

    Sign up first implementation

--- server/src/main/kotlin/it/polito/wa2/g34/server/security/SecurityConfiguration.kt
+++ server/src/main/kotlin/it/polito/wa2/g34/server/security/SecurityConfiguration.kt
@@ -1,8 +1,12 @@
 package it.polito.wa2.g34.server.security
 
 import it.polito.wa2.g34.server.profile.Role
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.admin.client.KeycloakBuilder
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -12,10 +16,13 @@ import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
 import org.springframework.security.web.SecurityFilterChain
 
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled=true, securedEnabled = true)
 class SecurityConfig {
+    @Autowired
+    lateinit var env: Environment
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf().disable()
@@ -27,6 +34,7 @@ class SecurityConfig {
             .requestMatchers(HttpMethod.POST, "/api/ticket/").hasRole(Role.CUSTOMER.name)
             .requestMatchers(HttpMethod.POST, "/api/ticket/*/reopen").hasRole(Role.CUSTOMER.name)
             .requestMatchers(HttpMethod.POST, "/api/login").permitAll()
+            .requestMatchers(HttpMethod.POST, "/api/signup").permitAll()
             .requestMatchers("/actuator/**").permitAll()
             .anyRequest().authenticated();
         http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter())
@@ -43,6 +51,19 @@ class SecurityConfig {
         converter.setPrincipalClaimName("email")
         return converter
 
+    }
+
+    @Bean
+    fun keycloak(): Keycloak {
+        val clientId = env.getProperty("keykloak.client.id")
+        val clientSecret = env.getProperty("keykloak.client.secret")
+        return KeycloakBuilder.builder()
+            .serverUrl("http://localhost:8080/")
+            .realm("master")
+            .username("admin")
+            .password("admin")
+            .clientId("admin-cli")
+            .build()
     }
 }
 
--- server/src/main/kotlin/it/polito/wa2/g34/server/security/SecurityController.kt
+++ server/src/main/kotlin/it/polito/wa2/g34/server/security/SecurityController.kt
@@ -3,6 +3,9 @@ package it.polito.wa2.g34.server.security
 import io.micrometer.observation.annotation.Observed
 import it.polito.wa2.g34.server.observability.LogInfo
 import jakarta.validation.Valid
+import org.keycloak.admin.client.Keycloak
+import org.keycloak.representations.idm.CredentialRepresentation
+import org.keycloak.representations.idm.UserRepresentation
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.http.*
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 data class UserLoginData(
     var username: String,
@@ -23,6 +27,8 @@ data class UserLoginData(
 @Observed
 @LogInfo
 class SecurityController() {
+    @Autowired
+    lateinit var keycloak: Keycloak
     @Autowired
     lateinit var env: Environment
 
@@ -57,4 +63,60 @@ class SecurityController() {
             throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials")
         }
     }
+
+    @PostMapping("/api/signup", produces = ["application/json"])
+    fun postSignUp(): String? {
+
+        val userRepresentation = UserRepresentation()
+        val credentialRepresentation = CredentialRepresentation()
+
+        userRepresentation.email = "pippo@topolino.it"
+        userRepresentation.username = "pippo@topolino.it"
+        userRepresentation.isEnabled = true
+        userRepresentation.realmRoles = listOf("CUSTOMER")
+
+        credentialRepresentation.type = CredentialRepresentation.PASSWORD;
+        credentialRepresentation.value = "password";
+        credentialRepresentation.isTemporary = false;
+        userRepresentation.credentials = listOf(credentialRepresentation);
+
+        return keycloak.realm("WA2_G34")
+            .users()
+            .create(userRepresentation).toString();
+
+        //return ""
+
+        /*
+        val issuerUri = env.getProperty("spring.security.oauth2.resourceserver.jwt.issuer-uri")
+        val clientId = env.getProperty("keykloak.client.id")
+        val clientSecret = env.getProperty("keykloak.client.secret")
+
+        val restTemplate = RestTemplate()
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_FORM_URLENCODED
+
+        val requestData = LinkedMultiValueMap<String, String>()
+        requestData.add("username", userLoginData!!.username)
+        requestData.add("password", userLoginData!!.password)
+        requestData.add("client_id", clientId)
+        requestData.add("client_secret", clientSecret)
+        requestData.add("grant_type", "password")
+
+        val requestEntity = HttpEntity(requestData, headers)
+        try {
+            val responseEntity = restTemplate.postForEntity(
+                "$issuerUri/protocol/openid-connect/token",
+                requestEntity,
+                String::class.java
+            )
+            return responseEntity.body
+        }
+        catch (e: HttpClientErrorException){
+            throw ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials")
+        }
+
+         */
+    }
+
+
 }
