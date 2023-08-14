be1d17accbcf1195702a784d3cd3cd8967b11720
justlooller <simonegiambrone2001.2@gmail.com>
2023-08-14 10:28:32 +0200

    client second version project start

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PAGES from "./pages/pages";
-import './globals.css';
-
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-
-
-const router = createBrowserRouter(PAGES);
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
