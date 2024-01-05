46ff8380c727f516a2f18be4a9a770cd7ff83a29
Giacomo Demattia <giacomo.demattia@studenti.polito.it>
2024-01-05 17:06:58 +0100

    Chat media and real time notification features

--- server/src/main/kotlin/it/polito/wa2/g34/server/ticketing/controller/TicketController.kt
+++ server/src/main/kotlin/it/polito/wa2/g34/server/ticketing/controller/TicketController.kt
@@ -1,23 +1,32 @@
 package it.polito.wa2.g34.server.ticketing.controller
 
  import io.micrometer.observation.annotation.Observed
-import it.polito.wa2.g34.server.observability.LogInfo
+ import it.polito.wa2.g34.server.observability.LogInfo
  import it.polito.wa2.g34.server.profile.ProfileService
  import it.polito.wa2.g34.server.ticketing.dto.*
-import it.polito.wa2.g34.server.ticketing.entity.Priority
-import it.polito.wa2.g34.server.ticketing.entity.State
-import it.polito.wa2.g34.server.ticketing.service.MessageService
-import it.polito.wa2.g34.server.ticketing.service.StateHistoryService
-import it.polito.wa2.g34.server.ticketing.service.TicketService
-import jakarta.validation.Valid
-import jakarta.validation.constraints.Email
-import org.slf4j.LoggerFactory
-import org.springframework.security.access.prepost.PreAuthorize
+ import it.polito.wa2.g34.server.ticketing.entity.Priority
+ import it.polito.wa2.g34.server.ticketing.entity.State
+ import it.polito.wa2.g34.server.ticketing.service.MessageService
+ import it.polito.wa2.g34.server.ticketing.service.StateHistoryService
+ import it.polito.wa2.g34.server.ticketing.service.TicketService
+ import jakarta.validation.Valid
+ import jakarta.validation.constraints.Email
+ import org.slf4j.LoggerFactory
+ import org.springframework.beans.factory.annotation.Autowired
+ import org.springframework.core.io.FileSystemResource
+ import org.springframework.http.MediaType
+ import org.springframework.messaging.simp.SimpMessagingTemplate
+ import org.springframework.security.access.prepost.PreAuthorize
  import org.springframework.security.core.context.SecurityContextHolder
  import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
-import java.security.Principal
-import java.time.LocalDateTime
+ import org.springframework.web.bind.annotation.*
+ import org.springframework.web.multipart.MultipartFile
+ import java.nio.file.Files
+ import java.nio.file.StandardCopyOption
+ import java.security.Principal
+ import java.util.*
+ import kotlin.io.path.Path
+
 
 @RestController
 @Validated
@@ -29,6 +38,10 @@ class TicketController(
     private val messageService: MessageService,
     private val profileService: ProfileService,
 ) {
+
+    @Autowired
+    private val messageSender: SimpMessagingTemplate? = null
+
 
     private val logger = LoggerFactory.getLogger(TicketController::class.java);
     @GetMapping("/api/ticket/{id}")
@@ -105,11 +118,35 @@ class TicketController(
 
     @PostMapping("/api/ticket/{id}/message")
     @PreAuthorize("#messageDTO.user_mail == authentication.name")
-    fun addMessageToTicket(@PathVariable("id") id: Long, @Valid @RequestBody messageDTO: MessageDTO) {
+    fun addMessageToTicket(@PathVariable("id") id: Long, @Valid @RequestPart messageDTO: MessageDTO, @RequestPart("document", required = false) file: MultipartFile?) {
+
+        if (file != null){
+            val dirPath = Path("messagesfiles/$id")
+            val fileName = UUID.randomUUID().toString()+'_'+file.originalFilename
+            val filePath = Path(dirPath.toString(),fileName)
+            if(!Files.exists(dirPath)){
+                Files.createDirectories(dirPath)
+            }
+            Files.copy(file.inputStream, filePath, StandardCopyOption.REPLACE_EXISTING)
+            messageDTO.attachment = fileName
+        }
         messageDTO.ticket_id = id
-        messageDTO.timestamp = LocalDateTime.now()
         messageService.sendMessage(messageDTO)
+        messageSender!!.convertAndSend("/chat/$id", messageDTO)
     }
+
+    @GetMapping("/api/ticket/{id}/message/{filename}",produces = [MediaType.APPLICATION_OCTET_STREAM_VALUE])
+    fun getMessageFile(@PathVariable("id") id: Long, @PathVariable("filename") filename: String) : FileSystemResource {
+            val dirPath = Path("messagesfiles/$id")
+            val fileName = filename
+            val filePath = Path(dirPath.toString(),fileName)
+            if(!Files.exists(filePath)){
+
+                throw Exception();
+            }
+            return FileSystemResource(filePath)
+        }
+
 
 
     @GetMapping("/api/ticket/{id}/messages")
@@ -123,4 +160,6 @@ class TicketController(
         val ticket = ticketService.getTicket(id)
         return stateHistoryService.getHistory(ticket.toDTO()).map { it.toDTO() }
     }
+
+
 }
